195e7ec1323b515f83a25ad3c602d51dea797dd2
staffeev <st.affeev@yandex.ru>
2024-04-10 23:45:42 +0300

    script fixed

--- core/preprocessing.py
+++ core/preprocessing.py
@@ -5,9 +5,10 @@ from torchvision import datasets
 
 def preprocessing_dataset(train_path="train_data", test_path="test_data"):
     transform_list = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Grayscale()
+        transforms.Resize(28),
+        transforms.ToTensor(),
+        transforms.Grayscale()
     ])
     train_images = datasets.ImageFolder(root=train_path, transform=transform_list)
-    test_images = datasets.ImageFolder(root=train_path, transform=transform_list)
+    test_images = datasets.ImageFolder(root=test_path, transform=transform_list)
     return train_images, test_images
--- scripts/training.py
+++ scripts/training.py
@@ -2,6 +2,7 @@ from .argument_parser import Parser
 import os
 import sys
 sys.path.append("..")
+import importlib.util
 from core.preprocessing import preprocessing_dataset
 from core.architecture import test_architecture, load_model_state
 from torch import nn
@@ -18,10 +19,14 @@ if __name__ == "__main__":
     if args.use_colab:
         os.chdir("/content")
     dataset_train, dataset_test = preprocessing_dataset(args.train_data_path, args.test_data_path)
-    module = __import__(args.model_path)
+
+    spec = importlib.util.spec_from_file_location("module", args.model_path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
     model_classname = [i for i in dir(module) if not i.startswith("_")][0]
+
     test_architecture(
-        dataset_train, dataset_test, eval(f"module.{model_classname}"), eval(f"optim.{args.optimiser}"),
-        eval(f"nn.{args.loss_func}"), args.num_epochs, args.batch_size, args.logging_iters_train,
+        dataset_train, dataset_test, eval(f"module.{model_classname}()"), eval(f"optim.{args.optimiser}"),
+        eval(f"nn.{args.loss_func}()"), args.num_epochs, args.batch_size, args.logging_iters_train,
         args.logging_iters_valid, args.model_title, args.save_graph, args.save_state, args.load_state
     )
