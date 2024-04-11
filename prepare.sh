9e83c1f5db3ecb501a9fbb463d0202ca5c02c84c
staffeev <st.affeev@yandex.ru>
2024-04-11 16:35:14 +0300

    prepare script

---
+++ prepare.sh
@@ -0,0 +1,10 @@
+flag=$1
+pip install opendatasets
+git init
+git clone https://ТОКЕН@github.com/staffeev/lungs_issues_ml.git
+cd lungs_issues_ml/
+if [[ -z "$flag" ]]; then
+    python3 scripts/creating_dataset.py --download
+else
+    python scripts/creating_dataset.py --download
+fi
