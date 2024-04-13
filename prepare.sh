8a4a4e9c9d70692c1ff4779630315b35315012a7
Rushan <149034339+rusha-mathing@users.noreply.github.com>
2024-04-13 21:26:58 +0500

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,8 @@
 flag=$1
 pip install opendatasets
-git init
-git clone https://github.com/staffeev/lungs_issues_ml.git
-cd lungs_issues_ml
+# git init  ### Я просто обычно уже репо через гит устанавливаю и поэтому это немного мешает. 
+# git clone https://github.com/staffeev/lungs_issues_ml.git
+# cd lungs_issues_ml
 if [[ -z "$flag" ]]; then
     python3 scripts/creating_dataset.py --download
 else
