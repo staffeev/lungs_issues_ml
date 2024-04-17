c897d05a6946ed61a562246f15d008235dc61c6b
rusha-mathing <rushan.is.into.math@gmail.com>
2024-04-17 16:54:35 +0500

    рушан исправил

--- models/unet.py
+++ models/unet.py
@@ -1,6 +1,5 @@
 import torch
 from torch import nn
-from torch.functional import F
 
 
 class UNet(nn.Module):
@@ -102,8 +101,8 @@ class upsampler(nn.Module):
         diffY = x2.size()[2] - x1.size()[2]
         diffX = x2.size()[3] - x1.size()[3]
 
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-                        diffY // 2, diffY - diffY // 2])
+        x1 = torch.functional.F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                                         diffY // 2, diffY - diffY // 2])
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
 
