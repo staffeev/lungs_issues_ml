65be5d2e40f674b32fb05cc162fa905482d3f010
rusha-mathing <rushan.is.into.math@gmail.com>
2024-04-17 22:42:56 +0500

    Слив предсказаний

---
+++ scripts/merge_predicts.py
@@ -0,0 +1,26 @@
+import sys
+import pandas as pd
+
+
+if __name__== '__main__':
+    result = sys.argv[1]
+
+    table = sum([pd.get_dummies(pd.read_csv(name), columns=['target_feature'], dtype=int) for name in sys.argv[2:]])
+
+    rows = []
+    features = [f'target_features_{i}' for i in range(3)]
+    for _, row in table.iterrows():
+        result_id = row.iloc[0]
+        result_target = row[1:].idxmax()
+        rows.append([result_id, result_target])
+    
+    result_df = pd.DataFrame(rows, columns=['id', 'target_feature'])
+    
+
+# Подсчет моды для каждого уникального значения Id
+#mode_predictions = merged_df.groupby('Id')['Target_Feature'].apply(lambda x: x.mode()[0])
+
+# Запись результата в выходной файл
+#output_file = 'most_common_predictions.csv'
+#mode_predictions.to_csv(output_file, header=['Target_Feature'])
+
