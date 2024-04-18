8e170ecc484820eef919a699bcddb3b65058c4d0
rusha-mathing <rushan.is.into.math@gmail.com>
2024-04-18 22:42:42 +0500

    Забыл классификатор убрать.

--- models/unet.py
+++ models/unet.py
@@ -33,8 +33,6 @@ class UNet(nn.Module):
         x = self.up3(x, x2)
         x = self.up4(x, x1)
         x = self.outc(x)
-        x = x.view(x.size()[0], -1)
-        x = self.classifier(x)
         return x
 
     def use_checkpointing(self):
