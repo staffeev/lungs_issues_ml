3275e1637ecde5ea83a4b06987b9908195b3c083
rusha-mathing <rushan.is.into.math@gmail.com>
2024-04-18 17:50:36 +0500

    Обновил версию с масками

--- scripts/mask_images.py
+++ scripts/mask_images.py
@@ -3,22 +3,6 @@ import os
 from tqdm import tqdm
 
 
-# Функция для загрузки изображений и их масок
-def load_images_and_masks(image_dir, mask_dir):
-    images = []
-    masks = []
-    for filename in os.listdir(image_dir):
-        if filename.endswith(".png"): 
-            image_path = os.path.join(image_dir, filename)
-            mask_path = os.path.join(mask_dir, filename)
-            
-            # Загрузка изображения и маски с помощью PIL
-            image = Image.open(image_path)  # Преобразуем изображение в RGB, так как маски - одноканальные
-            mask = Image.open(mask_path)  # Преобразуем маску в одноканальное черно-белое изображение
-            images.append(image)
-            masks.append(mask)
-    return images, masks
-
 # Функция для применения маски к изображению
 def apply_mask(image, mask):
     # Применение маски к изображению
@@ -38,10 +22,13 @@ if __name__ == '__main__':
     output_folder = "dataset/data/train_images_masked"
 
     # Загрузка изображений и их масок
-    images, masks = load_images_and_masks(image_folder, mask_folder)
-
-    # Применение масок к изображениям и сохранение результатов
-    for image, mask in tqdm(zip(images, masks), desc="Apply masks to image"):
-        masked_image = apply_mask(image, mask)
-        output_path = os.path.join(output_folder, os.path.basename(image.filename))
-        masked_image.save(output_path)
+    for filename in os.listdir(image_folder):
+        if filename.endswith(".png"): 
+            image_path = os.path.join(image_folder, filename)
+            mask_path = os.path.join(mask_folder, filename)
+            
+            image = Image.open(image_path)  # Преобразуем изображение в RGB, так как маски - одноканальные
+            mask = Image.open(mask_path)  # Преобразуем маску в одноканальное черно-белое изображение    for image, mask in tqdm(zip(images, masks), desc="Apply masks to image"):
+            masked_image = apply_mask(image, mask)
+            output_path = os.path.join(output_folder, os.path.basename(image.filename))
+            masked_image.save(output_path)
