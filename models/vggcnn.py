4a87aa2d46050d433481f11b4656a7132404dff9
staffeev <st.affeev@yandex.ru>
2024-04-19 23:28:52 +0300

    skips added

--- models/vggcnn.py
+++ models/vggcnn.py
@@ -1,6 +1,15 @@
 from torch import nn
 
 
+class block(nn.Module):
+    def __init__(self, *layers):
+        self.net = nn.Sequential(*layers)
+        self.identity = nn.Identity()
+    
+    def fordard(self, x):
+        return self.net(x) + self.identity(x)
+
+
 class VGGCNN(nn.Module):
     def __init__(self) -> None:
         super().__init__()
@@ -8,49 +17,61 @@ class VGGCNN(nn.Module):
             # 1
             nn.Conv2d(3, 64, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(True),
+            block(
+                nn.Conv2d(64, 64, kernel_size=3, padding=1),
+                nn.ReLU(True)
+            ),
             nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
             # 2
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.ReLU(True),
-            nn.Conv2d(128, 128, kernel_size=3, padding=1),
-            nn.ReLU(True),
+            block(
+                nn.Conv2d(128, 128, kernel_size=3, padding=1),
+                nn.ReLU(True)
+            ),
             nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
             # 3
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.ReLU(True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(True),
+            block(
+                nn.Conv2d(256, 256, kernel_size=3, padding=1),
+                nn.ReLU(True),
+                block(
+                    nn.Conv2d(256, 256, kernel_size=3, padding=1),
+                    nn.ReLU(True),
+                    nn.Conv2d(256, 256, kernel_size=3, padding=1),
+                    nn.ReLU(True)
+                ),
+            ),
             nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
             # 4
             nn.Conv2d(256, 512, kernel_size=3, padding=1),
             nn.ReLU(True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(True),
+            block(
+                nn.Conv2d(512, 512, kernel_size=3, padding=1),
+                nn.ReLU(True),
+                block(
+                    nn.Conv2d(512, 512, kernel_size=3, padding=1),
+                    nn.ReLU(True),
+                    nn.Conv2d(512, 512, kernel_size=3, padding=1),
+                    nn.ReLU(True)
+                ),
+            ),
             nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
             # 5
             nn.Conv2d(512, 512, kernel_size=3, padding=1),
             nn.ReLU(True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(True),
+            block(
+                nn.Conv2d(512, 512, kernel_size=3, padding=1),
+                nn.ReLU(True),
+                block(
+                    nn.Conv2d(512, 512, kernel_size=3, padding=1),
+                    nn.ReLU(True),
+                    nn.Conv2d(512, 512, kernel_size=3, padding=1),
+                    nn.ReLU(True)
+                ),
+            ),
             nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
-        )
-        self.a1 = nn.Sequential(
-            nn.Conv2d(512, 128, kernel_size=1),
-            nn.ReLU(True),
         )
         self.cnn_layers = nn.Sequential(
             nn.Conv2d(512, 128, kernel_size=1),
