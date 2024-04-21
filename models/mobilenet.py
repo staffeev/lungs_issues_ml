fd4f06ea2e4c69ed37fb475342d3e4fe813f43ac
staffeev <st.affeev@yandex.ru>
2024-04-21 21:16:24 +0300

    ввавып

--- models/mobilenet.py
+++ models/mobilenet.py
@@ -6,7 +6,7 @@ def dwise_conv(ch_in, stride=1):
         nn.Sequential(
             nn.Conv2d(ch_in, ch_in, kernel_size=3, padding=1, stride=stride, groups=ch_in, bias=False),
             nn.BatchNorm2d(ch_in),
-            nn.ReLU6(inplace=True),
+            nn.ReLU(inplace=True),
         )
     )
 
@@ -16,7 +16,7 @@ def conv1x1(ch_in, ch_out):
         nn.Sequential(
             nn.Conv2d(ch_in, ch_out, kernel_size=1, padding=0, stride=1, bias=False),
             nn.BatchNorm2d(ch_out),
-            nn.ReLU6(inplace=True)
+            nn.ReLU(inplace=True)
         )
     )
 
@@ -26,7 +26,7 @@ def conv3x3(ch_in, ch_out, stride):
         nn.Sequential(
             nn.Conv2d(ch_in, ch_out, kernel_size=3, padding=1, stride=stride, bias=False),
             nn.BatchNorm2d(ch_out),
-            nn.ReLU6(inplace=True)
+            nn.ReLU(inplace=True)
         )
     )
 
