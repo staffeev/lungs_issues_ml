42842c49e668bd7c8af4d4080f3578fda50fc298
staffeev <st.affeev@yandex.ru>
2024-04-21 17:22:05 +0300

    mobilenet try

---
+++ models/mobilenet.py
@@ -0,0 +1,104 @@
+import torch.nn as nn
+
+
+def dwise_conv(ch_in, stride=1):
+    return (
+        nn.Sequential(
+            nn.Conv2d(ch_in, ch_in, kernel_size=3, padding=1, stride=stride, groups=ch_in, bias=False),
+            nn.BatchNorm2d(ch_in),
+            nn.ReLU6(inplace=True),
+        )
+    )
+
+
+def conv1x1(ch_in, ch_out):
+    return (
+        nn.Sequential(
+            nn.Conv2d(ch_in, ch_out, kernel_size=1, padding=0, stride=1, bias=False),
+            nn.BatchNorm2d(ch_out),
+            nn.ReLU6(inplace=True)
+        )
+    )
+
+
+def conv3x3(ch_in, ch_out, stride):
+    return (
+        nn.Sequential(
+            nn.Conv2d(ch_in, ch_out, kernel_size=3, padding=1, stride=stride, bias=False),
+            nn.BatchNorm2d(ch_out),
+            nn.ReLU6(inplace=True)
+        )
+    )
+
+class invertedBlock(nn.Module):
+    def __init__(self, ch_in, ch_out, expand_ratio, stride):
+        super(invertedBlock, self).__init__()
+
+        self.stride = stride
+        assert stride in [1,2]
+
+        hidden_dim = ch_in * expand_ratio
+
+        self.use_res_connect = self.stride==1 and ch_in==ch_out
+
+        layers = []
+        if expand_ratio != 1:
+            layers.append(conv1x1(ch_in, hidden_dim))
+        layers.extend([
+            #dw
+            dwise_conv(hidden_dim, stride=stride),
+            #pw
+            conv1x1(hidden_dim, ch_out)
+        ])
+
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        if self.use_res_connect:
+            return x + self.layers(x)
+        else:
+            return self.layers(x)
+
+
+class MobileNet(nn.Module):
+    def __init__(self, ch_in=3, n_classes=3):
+        super(MobileNet, self).__init__()
+
+        self.configs=[
+            # t, c, n, s
+            [1, 16, 1, 1],
+            [6, 24, 2, 2],
+            [6, 32, 3, 2],
+            [6, 64, 4, 2],
+            [6, 96, 3, 1],
+            [6, 160, 3, 2],
+            [6, 320, 1, 1]
+        ]
+
+        self.stem_conv = conv3x3(ch_in, 32, stride=2)
+
+        layers = []
+        input_channel = 32
+        for t, c, n, s in self.configs:
+            for i in range(n):
+                stride = s if i == 0 else 1
+                layers.append(invertedBlock(ch_in=input_channel, ch_out=c, expand_ratio=t, stride=stride))
+                input_channel = c
+
+        self.layers = nn.Sequential(*layers)
+
+        self.last_conv = conv1x1(input_channel, 1280)
+
+        self.classifier = nn.Sequential(
+            nn.Dropout2d(0.2),
+            nn.Linear(1280, n_classes)
+        )
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+
+    def forward(self, x):
+        x = self.stem_conv(x)
+        x = self.layers(x)
+        x = self.last_conv(x)
+        x = self.avg_pool(x).view(-1, 1280)
+        x = self.classifier(x)
+        return x
