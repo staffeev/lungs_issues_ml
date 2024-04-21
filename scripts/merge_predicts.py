fef98e759e3941139724e30dfcebe1906bb3a35b
rusha-mathing <rushan.is.into.math@gmail.com>
2024-04-21 16:49:33 +0500

    Исправил скрипт по сливанию.

--- scripts/merge_predicts.py
+++ scripts/merge_predicts.py
@@ -7,15 +7,18 @@ if __name__== '__main__':
     result = sys.argv[1]
 
     table = sum([pd.get_dummies(pd.read_csv(name), columns=['target_feature'], dtype=int) for name in sys.argv[2:]])
+    table['id'] = table['id'] // (len(sys.argv) - 2)
 
     rows = []
     features = [f'target_features_{i}' for i in range(3)]
     for _, row in tqdm(table.iterrows(), desc="Merge predictions"):
         result_id = row.iloc[0]
-        result_target = row[1:].idxmax()
+        result_target = int(row[1:].idxmax()[-1])
         rows.append([result_id, result_target])
     
     result_df = pd.DataFrame(rows, columns=['id', 'target_feature'])
+
+    result_df.to_csv(result, index=False, index_label=False)
     
 
 # Подсчет моды для каждого уникального значения Id
